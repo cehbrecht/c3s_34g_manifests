1e02c5e7d37c6a9668aac355e65b9ca494be4d62
Carsten Ehbrecht <ehbrecht@dkrz.de>
2021-03-13 17:19:27 +0100

    update catalog builder

--- intake/catalog_builder.py
+++ intake/catalog_builder.py
@@ -1,6 +1,9 @@
 import os
 import yaml
 from pathlib import Path
+from tqdm import tqdm
+
+import pandas as pd
 
 here = Path(os.path.dirname(__file__))
 
@@ -13,5 +16,33 @@ def read_inventory():
     return inv
 
 
+def build_catalog(inventory):
+    entries = []
+    for item in tqdm(inventory):
+        try:
+            if "path" in item:
+                for filename in item["files"]:
+                    entry = {
+                        "ds_id": item["ds_id"],
+                        "path": f'{item["path"]}/{filename}',
+                    }
+                    entries.append(entry)
+        except ValueError:
+            print(f"Error: {item}")
+    print("Build Dataframe ...")
+    df = pd.DataFrame(entries)
+    columns = [
+        'ds_id',
+        'path',
+    ]
+    return df[columns]
+
+
 if __name__ == '__main__':
+    print("Loading inventory ...")
     inv = read_inventory()
+    print("Building catalog ...")
+    df = build_catalog(inv)
+    cat_name = here / "catalogs" / "c3s-cmip6_latest.csv"
+    df.to_csv(cat_name, index=False)
+    print(f"Catalog written: {cat_name}")
